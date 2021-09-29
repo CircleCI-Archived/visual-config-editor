4813d38121143bec3270fa9232a13796ccd88bf8
Hamza Naqi <sardan5@vt.edu>
2021-09-29 11:06:06 -0400

    split-pane, Form & Map

---
+++ src/helpers/Position.ts
@@ -0,0 +1,14 @@
+export function Position(elements: {}[]) {
+    if (elements.length === 0) {
+      return {
+        x: 100,
+        y: 200,
+      }
+    }
+    else {
+      return {
+        x: 280,
+        y: 180,
+      }
+    }
+  }
