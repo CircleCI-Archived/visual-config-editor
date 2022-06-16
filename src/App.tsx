96797c7b2c58ff8cb11ea13f64ab7d6187d8e7a8
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-16 17:03:08 -0400

    fix: Prevent Store Duplication Upon App Refresh (#124)
    * feat: added step definition nav
    
    * fix: store no longer refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import WorkflowsPane from './components/panes/WorkflowsPane';
 import useWindowDimensions from './state/Hooks';
 import Store from './state/Store';
 
+const store = createStore(Store);
+
 const App = () => {
   const inspectorWidth = 320;
   const appWidth = useWindowDimensions();
 
   return (
-    <StoreProvider store={createStore(Store)}>
+    <StoreProvider store={store}>
       <section className="flex flex-row h-full">
         <section
           className="flex flex-col flex-nowrap flex-1"
