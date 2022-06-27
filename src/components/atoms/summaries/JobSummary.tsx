eddcf506ae0c85aca19c352dfacd16382d346c7a
Jaryt Bustard <jarytbustard@gmail.com>
2022-06-27 16:23:06 -0400

    chore: remove console log

--- src/components/atoms/summaries/JobSummary.tsx
+++ src/components/atoms/summaries/JobSummary.tsx
@@ -8,15 +8,18 @@ const JobSummary: React.FunctionComponent<{ data: Job }> = (props) => {
   const workflows = useStoreState((state) => state.workflows);
   const selectedWorkflow = useStoreState((state) => state.selectedWorkflow);
   const workflowName = workflows[selectedWorkflow].name;
-  const stagedJobs = stagedJobMap.workflows[workflowName]
-
-  console.log(stagedJobs && Object.keys(stagedJobs).includes(props.data.name), stagedJobs && props.data.name in stagedJobs)
+  const stagedJobs = stagedJobMap.workflows[workflowName];
 
   return (
     <div className="flex flex-row">
       <JobIcon className="ml-1 mr-2 w-5 h-5" />
       <p className="leading-5">{props.data.name}</p>
-      {stagedJobs && <WorkflowIcon className="w-5 h-5 ml-auto" color={props.data.name in stagedJobs ? '#048C43' : '#E3E3E3'} />}
+      {stagedJobs && (
+        <WorkflowIcon
+          className="w-5 h-5 ml-auto"
+          color={props.data.name in stagedJobs ? '#048C43' : '#E3E3E3'}
+        />
+      )}
     </div>
   );
 };
