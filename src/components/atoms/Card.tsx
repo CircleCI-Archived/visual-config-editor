527b8bea04bc2f27310144f3522ee054a60400a3
Jaryt Bustard <jarytbustard@gmail.com>
2022-07-05 17:59:42 -0400

    feat: added pagination to orb menu

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -3,25 +3,32 @@ export interface CardProps {
   title: string;
   icon?: React.ReactElement;
   pinned?: React.ReactElement;
+  truncate?: number;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Card = (props: CardProps) => {
+const Card = ({ truncate, description, ...props }: CardProps) => {
   return (
-    <button
-      type="button"
-      className="p-4 mb-4 w-full border-circle-gray-300 border-2 rounded text-left"
-      onClick={props.onClick}
-    >
-      <div className="flex flex-row">
-        {props.icon}
-        <p className="font-bold">{props.title}</p>
-        <div className="ml-auto z-10">{props.pinned}</div>
-      </div>
-      <p className="text-sm mt-1 leading-4 text-circle-gray-500 overflow-ellipsis">
-        {props.description}
-      </p>
-    </button>
+    <div className="pb-4">
+      <button
+        type="button"
+        className="p-4 w-full border-circle-gray-300 border rounded text-left hover:border-circle-gray-700 max-h-36"
+        onClick={props.onClick}
+      >
+        <div className="flex flex-row">
+          {props.icon}
+          <p className="font-bold">{props.title}</p>
+          <div className="ml-auto z-10">{props.pinned}</div>
+        </div>
+        {description && (
+          <p className="text-sm mt-1 leading-4 text-circle-gray-500 overflow-ellipsis max-h-32 overflow-hidden">
+            {truncate && description.length > truncate
+              ? description?.slice(0, truncate) + '...'
+              : description}
+          </p>
+        )}
+      </button>
+    </div>
   );
 };
 
