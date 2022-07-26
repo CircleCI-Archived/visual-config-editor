575f0d1c64bdb54c88833444bf00d14d8b917c44
Jaryt Bustard <jarytbustard@gmail.com>
2022-07-26 12:20:41 -0400

    feat: Staged Job pre/post-steps (#195)
    * feat: pre/post steps implementation
    
    * fix: pass getters and setters to step definition menu
    
    * chore: rebased to main
    
    * feat: finished pre and post steps
    
    Co-authored-by: James <35617601+jliby@users.noreply.github.com>

---
+++ src/components/atoms/form/AdjacentStepListItem.tsx
@@ -0,0 +1,46 @@
+import { useStoreActions } from '../../../state/Hooks';
+import { StepDefinitionMenuNav } from '../../menus/definitions/StepDefinitionMenu';
+import { ListItemChildProps } from './ListProperty';
+
+export type AdjacentStepLiteral = 'pre-steps' | 'post-steps';
+
+const AdjacentStepListItem = ({
+  item,
+  index,
+  values,
+  type,
+}: ListItemChildProps & { type: AdjacentStepLiteral }) => {
+  const navigateTo = useStoreActions((actions) => actions.navigateTo);
+  const commandName = Object.keys(item)[0];
+  const commandValues = item[commandName];
+
+  return (
+    <button
+      className="flex-1 cursor-pointer text-left text-circle-black leading-6"
+      type="button"
+      onClick={() => {
+        navigateTo({
+          component: StepDefinitionMenuNav,
+          props: {
+            editing: true,
+            values: {
+              name: commandName,
+              parameters: commandValues,
+            },
+            getter: (values: any) => values.parameters[type],
+            setter: (values: any, value: any) =>
+              (values.parameters[type] = value),
+            index,
+          },
+          values: {
+            ...values,
+          },
+        });
+      }}
+    >
+      {commandName}
+    </button>
+  );
+};
+
+export default AdjacentStepListItem;
