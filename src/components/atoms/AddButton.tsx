b1ffa8487d7b5ab5e079d7a5200df3ba4f16411a
Jaryt Bustard <jarytbustard@gmail.com>
2022-08-16 18:53:50 -0400

    fix: disable add job button instead of hiding (#236)

--- src/components/atoms/AddButton.tsx
+++ src/components/atoms/AddButton.tsx
@@ -4,11 +4,12 @@ import AddIcon from '../../icons/ui/AddIcon';
 const AddButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
+
       type="button"
       {...props}
       className={
-        'bg-circle-gray-300 hover:bg-circle-gray-400 transition-colors h-8 w-8 rounded ' +
-        props.className
+        `bg-circle-gray-300 transition-colors h-8 w-8 rounded ${props.className} 
+      ${props.disabled ? 'opacity-50 cursor-default' : 'hover:bg-circle-gray-400 '}`
       }
     >
       <AddIcon className="m-auto"></AddIcon>
