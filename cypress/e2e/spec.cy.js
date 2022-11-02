4923cb740a547119dcaebb05a02d9e1c735014a1
Jaryt <jarytbustard@gmail.com>
2022-11-02 15:06:38 -0400

    ci: added test job

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,7 @@
 describe('Components Mount', () => {
     it('Should have a title header', () => {
-        cy.visit('http://127.0.0.1:5174/')
-
-        const val = cy.get('#root > section > section').find('h1').then((h1) => {
+        cy.visit('http://127.0.0.1:5174/');
+        cy.get('#root > section > section').find('h1').then((h1) => {
             expect(h1.text()).equal('Visual Config Editor');
         });
     })
