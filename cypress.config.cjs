c95f04068f9b4511d72cfdb2cccfd9e75e10885d
Jaryt Bustard <jarytbustard@gmail.com>
2022-11-02 17:33:29 -0400

    ci: added test job (#295)

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5174',
+    baseUrl: 'http://localhost:5173',
     supportFile: false,
   }
 })
