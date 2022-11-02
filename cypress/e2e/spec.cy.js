53a4f2cae13111dfe9fee0183e89cf32fe742b00
Jaryt <jarytbustard@gmail.com>
2022-11-02 13:31:47 -0400

    test: added base test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,9 @@
 describe('Components Mount', () => {
     it('Should have a title header', () => {
-        const val = cy.get('body').get('#root').get('section')
+        cy.visit('http://127.0.0.1:5174/')
 
-        console.log(val)
+        const val = cy.get('#root > section > section').find('h1').then((h1) => {
+            expect(h1.text()).equal('Visual Config Editor');
+        });
     })
 })
